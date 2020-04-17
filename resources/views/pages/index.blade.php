8b5c1a50c48b89d17cf9cd10e80f19b1c4395f7f
Mike Galante <mgalante324@gmail.com>
2020-04-17 15:19:47 -0400

    Setup the index page design. Added a layout and stylesheet specific to the index. Added the font used for the logo.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,1 +1,19 @@
-INDEX
+@extends('layouts.index')
+
+@section('content')
+<div class="container-fluid">
+  <div class="content">
+        {{-- <img src="{{ asset('../images/PrimeTimeGraphics_AltLogo3.png') }}" class="img-fluid" alt="Logo"> --}}
+        <img src="{{ asset('../images/PrimeTimeGraphics_Logo.png') }}" class="img-fluid" alt="PrimeTime Graphics">
+        {{-- <h1>PRIMETIME</h1>
+        <h3>GRAPHICS</h3> --}}
+
+    <div class="links">
+      <a href="/portfolio"><div class="link-text">Portfolio</div></a>
+      <a href="/about"><div class="link-text">About</div></a>
+      <a href="/shop"><div class="link-text">Shop</div></a>
+      <a href="/contact"><div class="link-text">Contact</div></a>
+    </div>
+  </div>
+</div>
+@endsection
