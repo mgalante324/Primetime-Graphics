f2fc59ef5bb2c52e3abaa1d0c8d4d63c465247cf
Mike Galante <mgalante324@gmail.com>
2020-04-20 15:12:14 -0400

    Style changes to the portfolio page.

--- resources/views/pages/portfolio.blade.php
+++ resources/views/pages/portfolio.blade.php
@@ -20,7 +20,7 @@
             <!-- Logo -->
             <div class="logo-title">
               <a href="/" class="logo">
-                <span class="symbol"><img src="{{ asset('phantom/images/logo.svg') }}" alt="" /></span><span class="title">PrimeTime Graphics</span>
+                <span class="symbol"><img src="{{ asset('phantom/images/logo.svg') }}" alt="" /></span><span class="title">PrimeTime Graphic</span>
               </a>
             </div>
 
@@ -32,8 +32,7 @@
   				</nav>
 
           <header>
-            <h1 class="title">PrimeTime Graphics</h1>
-            <p class="subtitle">Etiam quis viverra lorem, in semper lorem. Sorcisit amet nullam dolore.</p>
+            <h1 class="title">Portfolio</h1>
           </header>
 
   		</div>
@@ -133,35 +132,6 @@
   <!-- Footer -->
   	<footer id="footer">
   		<div class="inner">
-  			<section>
-  				<h2>Get in touch</h2>
-  				<form method="post" action="#">
-  					<div class="fields">
-  						<div class="field half">
-  							<input type="text" name="name" id="name" placeholder="Name" />
-  						</div>
-  						<div class="field half">
-  							<input type="email" name="email" id="email" placeholder="Email" />
-  						</div>
-              <div class="field">
-                <input type="text" name="subject" id="subject" placeholder="Subject"></input>
-              </div>
-  						<div class="field">
-  							<textarea name="message" id="message" placeholder="Message"></textarea>
-  						</div>
-  					</div>
-  					<ul class="actions">
-  						<li><input type="submit" value="Send" class="primary" /></li>
-  					</ul>
-  				</form>
-  			</section>
-  			<section>
-  				<h2>Follow</h2>
-  				<ul class="icons">
-  					<li><a href="https://www.instagram.com/primetime_graphic/" class="icon brands style2 fa-instagram"><span class="label">Instagram</span></a></li>
-  					<li><a href="#" class="icon brands style2 fa-twitter"><span class="label">Twitter</span></a></li>
-  				</ul>
-  			</section>
   			<ul class="copyright">
   				<li>&copy; PrimeTime Graphics 2020. All rights reserved</li>
   			</ul>
