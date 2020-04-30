daebda93cacbec2943e5e617db832dac55f3a03d
Mike Galante <mgalante324@gmail.com>
2020-04-30 16:51:37 -0400

    Edited the contact form.

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -53,34 +53,43 @@
              @endguest
        	</nav>
 
+        <!-- Main -->
+          <div id="main">
+            <div class="inner">
+              <div class="contactForm">
+                  <section>
+                    <h1 class="title">Contact</h1>
+                    <form method="post" action="#">
+                      <div class="fields">
+                        <div class="field half">
+                          <input type="text" name="name" id="name" placeholder="Name" />
+                        </div>
+                        <div class="field half">
+                          <input type="email" name="email" id="email" placeholder="Email" />
+                        </div>
+                        <div class="field">
+                          <input type="text" name="subject" id="subject" placeholder="Subject"></input>
+                        </div>
+                        <div class="field">
+                          <textarea name="message" id="message" placeholder="Message"></textarea>
+                        </div>
+                      </div>
+                      <ul class="actions">
+                        <li><input type="submit" value="Send" class="button primary" /></li>
+                      </ul>
+                    </form>
+                    <ul class="icons">
+                      <li><a href="https://www.instagram.com/primetime_graphic/" class="icon brands style2 fa-instagram"><span class="label">Instagram</span></a></li>
+                      {{-- <li><a href="#" class="icon brands style2 fa-twitter"><span class="label">Twitter</span></a></li> --}}
+                    </ul>
+                  </section>
+                </div>
+            </div>
+          </div>
+
       <!-- Footer -->
         <footer id="footer">
           <div class="inner">
-            <section>
-              <form method="post" action="#">
-                <div class="fields">
-                  <div class="field half">
-                    <input type="text" name="name" id="name" placeholder="Name" />
-                  </div>
-                  <div class="field half">
-                    <input type="email" name="email" id="email" placeholder="Email" />
-                  </div>
-                  <div class="field">
-                    <input type="text" name="subject" id="subject" placeholder="Subject"></input>
-                  </div>
-                  <div class="field">
-                    <textarea name="message" id="message" placeholder="Message"></textarea>
-                  </div>
-                </div>
-                <ul class="actions">
-                  <li><input type="submit" value="Send" class="button primary" /></li>
-                </ul>
-              </form>
-              <ul class="icons">
-                <li><a href="https://www.instagram.com/primetime_graphic/" class="icon brands style2 fa-instagram"><span class="label">Instagram</span></a></li>
-                {{-- <li><a href="#" class="icon brands style2 fa-twitter"><span class="label">Twitter</span></a></li> --}}
-              </ul>
-            </section>
             <ul class="copyright">
               <li>&copy; PrimeTime Graphics 2020. All rights reserved</li>
             </ul>
