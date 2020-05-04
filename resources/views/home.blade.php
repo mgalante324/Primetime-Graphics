62831a571d8c257773c457f3be7ba340e89fadcc
Mike Galante <mgalante324@gmail.com>
2020-05-04 10:36:19 -0400

    Made table of projects responsive.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,8 +40,8 @@
 
 
             <!-- Projects Card -->
-          <div class="card" style="margin-top: 25px;">
-              <div class="card-header">{{ $projectCount }} Projects</div>
+          <div class="card" style="margin-top: 25px; margin-bottom: 50px;">
+            <div class="card-header">{{ $projectCount }} Projects</div>
             <div class="card-body">
                   @if (session('status'))
                       <div class="alert alert-success" role="alert">
@@ -49,35 +49,44 @@
                       </div>
                   @endif
                   <a href="/projects/create" class="btn btn-primary" style="margin-bottom: 25px;">Add a Project</a>
-                  <table class="table table-striped">
-                    <tr>
-                      <th>Name</th>
-                      <th>Category</th>
-                      <th>View</th>
-                      <th>Edit</th>
-                      <th>Delete</th>
-                    </tr>
-                    @foreach ($projects as $project)
+                  <div class="table-responsive">
+                    <table class="table table-striped">
                       <tr>
-                        <td>{{ $project->name }}</td>
-                        <td>{{ $project->category }}</td>
-                        <td><a href="/portfolio/{{ $project->category }}/{{ $project->id }}" class="btn btn-success">View</a></td>
-                        <td><a href="/projects/{{ $project->id }}/edit" class="btn btn-primary">Edit</a></td>
-                        <td>
-                          <form method="post" action="{{ route('projects.destroy', $project->id) }}">
-                              @csrf
-                              @method('DELETE')
-                                  <input type="submit" value="Delete" class="btn btn-danger" />
-                          </form>
-                        </td>
+                        <th>Name</th>
+                        <th>Category</th>
+                        <th>View</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
                       </tr>
-                    @endforeach
-                  </table>
+                      @foreach ($projects as $project)
+                        <tr>
+                          <td>{{ $project->name }}</td>
+                          <td>{{ $project->category }}</td>
+                          <td><a href="/portfolio/{{ $project->category }}/{{ $project->id }}" class="btn btn-success">View</a></td>
+                          <td><a href="/projects/{{ $project->id }}/edit" class="btn btn-primary">Edit</a></td>
+                          <td>
+                            <form method="post" action="{{ route('projects.destroy', $project->id) }}">
+                                @csrf
+                                @method('DELETE')
+                                    <input type="submit" value="Delete" class="btn btn-danger" />
+                            </form>
+                          </td>
+                        </tr>
+                      @endforeach
+                    </table>
+                  </div>
             </div>
           </div>
 
         </div>
     </div>
+
+  <!-- Footer -->
+  <footer class="footer" style="margin-bottom: 25px;">
+    <div class="container" style="text-align: center;">
+      <span class="text-muted">&copy; PrimeTime Graphics 2020. All rights reserved</span>
+    </div>
+  </footer>
 
 </div>
 
